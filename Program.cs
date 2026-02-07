222586e00895a3c2cf7d551dd50b639b4dfb6e91
Clayton Prebelli <clayton@prebellisolucoes.com>
2026-02-06 23:48:54 -0300

    swagger

--- Program.cs
+++ Program.cs
@@ -51,11 +51,9 @@ builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
