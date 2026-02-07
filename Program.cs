a8d76e1588aad3a02204687a8156280e3261a357
Clayton Prebelli <clayton@prebellisolucoes.com>
2026-02-06 23:57:35 -0300

    swagger

--- Program.cs
+++ Program.cs
@@ -53,7 +53,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
     app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiRmBoutique v1");
+    c.RoutePrefix = "swagger"; // acessa em /swagger
+});
+
 
 app.UseHttpsRedirection();
 
